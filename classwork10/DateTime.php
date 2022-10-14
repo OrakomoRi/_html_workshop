bd750b43a772205a012ab7d2ad13bddad3d7dedb
Yukio Oramoshi <60033555+OrakomoRi@users.noreply.github.com>
2022-10-14 04:41:21 +0300

    Task 1 complete!

---
+++ classwork10/DateTime.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Classwork 10</title>
+	<meta name="author" content="OrakomoRi">
+	<link rel="icon" href="https://i.imgur.com/wIbsb0z.png">
+</head>
+<body>
+	<?php
+		echo 'Date & time: ' . date("d.m.Y & h:i");
+	?>
+</body>
+</html>
