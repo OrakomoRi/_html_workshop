40c556a7ad7e0f9829db197b72d693801781c402
Yukio Oramoshi <60033555+OrakomoRi@users.noreply.github.com>
2022-10-14 04:47:00 +0300

    Update

--- classwork10/DateTime.php
+++ classwork10/DateTime.php
@@ -2,9 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Classwork 10</title>
+	<title>Classwork 10 Task 1</title>
 	<meta name="author" content="OrakomoRi">
 	<link rel="icon" href="https://i.imgur.com/wIbsb0z.png">
 </head>
